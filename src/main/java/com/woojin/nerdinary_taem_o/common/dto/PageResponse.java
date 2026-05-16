b3dd0870251c692fc500f26ea37bbf4cb94236af
woogym <kwj9294@gmail.com>
2026-05-16 23:01:42 +0900

    chore: PageResponse추가

---
+++ src/main/java/com/woojin/nerdinary_taem_o/common/dto/PageResponse.java
@@ -0,0 +1,55 @@
+package com.woojin.nerdinary_taem_o.common.dto;
+
+import java.util.List;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+@Getter
+public class PageResponse<T> {
+
+    private final List<T> content;
+    private final int page;
+    private final int size;
+    private final long totalElements;
+    private final int totalPages;
+
+    public PageResponse(Page<T> page) {
+        this.content = page.getContent();
+        this.page = page.getNumber();
+        this.size = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+    }
+
+    public static <T, R> PageResponse<R> of(Page<T> page, List<R> content) {
+        return new PageResponse<>(
+                content,
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages()
+        );
+    }
+
+    public static <T> PageResponse<T> of(List<T> content,
+                                         int page,
+                                         int size,
+                                         long totalElements,
+                                         int totalPages
+    ) {
+        return new PageResponse<>(content, page, size, totalElements, totalPages);
+    }
+
+    private PageResponse(List<T> content,
+                         int page,
+                         int size,
+                         long totalElements,
+                         int totalPages) {
+        this.content = content;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+    }
+}
+
