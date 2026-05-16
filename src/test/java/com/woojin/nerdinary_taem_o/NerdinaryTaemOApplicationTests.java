fb6cd8c59e2a72d3ab169738d71e803f08bdb04c
SUNG HYUNJOON <tjdguswns1941@gachon.ac.kr>
2026-05-17 06:46:57 +0900

    .

--- src/test/java/com/woojin/nerdinary_taem_o/NerdinaryTaemOApplicationTests.java
+++ src/test/java/com/woojin/nerdinary_taem_o/NerdinaryTaemOApplicationTests.java
@@ -2,7 +2,7 @@ package com.woojin.nerdinary_taem_o;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// 테스트 코드
 @SpringBootTest
 class NerdinaryTaemOApplicationTests {
 
